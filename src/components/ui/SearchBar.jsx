2f9f6355ab3dd322eb0fbbb46d8ec5bd83ff1c7b
omar <omarfarah2002@hotmail.com>
2025-05-22 20:11:27 +0200

    created the input field for searchbar

---
+++ src/components/ui/SearchBar.jsx
@@ -0,0 +1,13 @@
+function SearchBar() {
+    return (
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search recipes..."
+          className="search-input"
+        />
+      </div>
+    );
+  }
+  
+  export default SearchBar;
