e5d844ae4a0119bd4223a420e60fb9c23a47e6d7
omar <omarfarah2002@hotmail.com>
2025-05-23 15:54:27 +0200

    fixed the user alert correctly

---
+++ src/components/ui/RandomRecipeButton.jsx
@@ -0,0 +1,14 @@
+function RandomRecipeButton() {
+    const handleClick = () => {
+      alert("This will later show a random recipe");
+    };
+  
+    return (
+      <div className="random-button">
+        <button onClick={handleClick}>🎲 Random Recipe</button>
+      </div>
+    );
+  }
+  
+  export default RandomRecipeButton;
+  
