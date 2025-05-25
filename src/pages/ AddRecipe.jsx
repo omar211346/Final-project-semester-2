7df2a127abc2581fca0a5b54370a683058bdae7b
omar <omarfarah2002@hotmail.com>
2025-05-25 19:57:28 +0200

    implemented formhandling

--- src/pages/ AddRecipe.jsx
+++ src/pages/ AddRecipe.jsx
@@ -1,12 +1,29 @@
-import RecipeForm from "../components/recipe/RecipeForm"; 
+import RecipeForm from "../components/recipe/RecipeForm";
 
 function AddRecipe() {
-    return (
-      <div className="add-recipe-page">
-        <h2>Add New Recipe</h2>
-      </div>
-    );
-  }
-  
-  export default AddRecipe;
-  
+  const handleSubmit = () => {
+    const form = document.getElementById("recipe-form");
+
+    const newRecipe = {
+      title: form.title,
+      category: form.category,
+      time: form.time,
+      difficulty: form.difficulty,
+      ingredients: form.ingredients,
+      instructions: form.instructions,
+      image: form.image,
+      createdAt: Date.now,
+    };
+
+    console.log("Recipe submitted:", newRecipe);
+  };
+
+  return (
+    <div className="add-recipe-page">
+      <h2>Add New Recipe</h2>
+      <RecipeForm onSubmit={handleSubmit} />
+    </div>
+  );
+}
+
+export default AddRecipe;
