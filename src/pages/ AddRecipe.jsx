ad0dd2226f88ae7300090a41e8200f0246b23f34
omar <omarfarah2002@hotmail.com>
2025-05-25 18:45:16 +0200

    triyng to import recipeform

---
+++ src/pages/ AddRecipe.jsx
@@ -0,0 +1,12 @@
+import RecipeForm from "../components/recipe/RecipeForm"; 
+
+function AddRecipe() {
+    return (
+      <div className="add-recipe-page">
+        <h2>Add New Recipe</h2>
+      </div>
+    );
+  }
+  
+  export default AddRecipe;
+  
