3d0350e0eea10166b6ad2ae388cebfdd0076e21d
omar <omarfarah2002@hotmail.com>
2025-05-25 18:26:31 +0200

    add title, category, and time fields

--- src/components/recipe/RecipeForm.jsx
+++ src/components/recipe/RecipeForm.jsx
@@ -1,7 +1,18 @@
 function RecipeForm({ onSubmit }) {
     return (
       <form className="recipe-form" onSubmit={onSubmit}>
+            <input type="text" placeholder="Title" name="title" required />
 
+         <select name="category" required>
+             <option value="">Select category</option>
+             <option value="Dinner">Dinner</option>
+             <option value="Dessert">Dessert</option>
+             <option value="Breakfast">Breakfast</option>
+             <option value="Vegetarian">Vegetarian</option>
+             <option value="Snack">Snack</option>
+         </select>
+
+            <input type="number" placeholder="Time (minutes)" name="time" required />
       </form>
     );
   }
