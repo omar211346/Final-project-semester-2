84028e11a28066008bf8ae657a7b447148a0407c
omar <omarfarah2002@hotmail.com>
2025-05-26 14:43:12 +0200

    created the page with header

---
+++ src/pages/RecipeDetails.jsx
@@ -0,0 +1,10 @@
+function RecipeDetails() {
+    return (
+      <div className="recipe-details-page">
+        <h2>Recipe Details</h2>
+      </div>
+    );
+  }
+  
+  export default RecipeDetails;
+  
