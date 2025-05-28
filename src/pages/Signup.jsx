56bcec940fb99a6f0c85a26e5770cfddb1983276
omar <omarfarah2002@hotmail.com>
2025-05-28 16:34:24 +0200

    added the sign up forms with 3 input fields

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,6 +2,12 @@ function Signup() {
     return (
       <div className="signup-page">
         <h2>Signup</h2>
+        <form>
+          <input type="text" name="username" placeholder="Username" required />
+          <input type="email" name="email" placeholder="Email" required />
+          <input type="password" name="password" placeholder="Password" required />
+          <button type="submit">Sign up</button>
+        </form>
       </div>
     );
   }
