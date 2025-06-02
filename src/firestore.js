2dacfa7bff2b4f1216158d57eda6bb3a8d5d6804
omar <omarfarah2002@hotmail.com>
2025-06-02 13:56:35 +0200

    add addRecipe() function in firestore.js to save recipes to Firestore

---
+++ src/firestore.js
@@ -0,0 +1,13 @@
+t
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../firebaseConfig";
+
+export async function addRecipe(recipe) {
+  try {
+    const docRef = await addDoc(collection(db, "recipes"), recipe);
+    return docRef.id;
+  } catch (error) {
+    console.error("Error adding recipe:", error);
+    throw error;
+  }
+}
