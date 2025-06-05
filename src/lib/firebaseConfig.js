5b9b6447c19f68ac6176be782bdd15df46e32a26
omar <omarfarah2002@hotmail.com>
2025-06-05 07:03:45 +0200

    had to create the .env and reconfigure firebase to finish the deployment

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -1,17 +1,15 @@
-
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA6GugTdkLtueCK2uGV7YxW-xWN0es8rUM",
-  authDomain: "oppskriftshub.firebaseapp.com",
-  projectId: "oppskriftshub",
-  storageBucket: "oppskriftshub.appspot.com",
-  messagingSenderId: "348966582683",
-  appId: "1:348966582683:web:38d8e12eb1eef60b75493e"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
-
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
