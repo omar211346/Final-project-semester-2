4715c2b67cc184a8a6210d168daf88449f21c7f0
omar <omarfarah2002@hotmail.com>
2025-06-05 07:24:21 +0200

    surely this has to be right, netlify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { getAllRecipes } from "../lib/firestore";
 import RecipeCard from "../components/recipe/RecipeCard";
-import '../css/home.css';
+import "../styles/home.css";
+
 
 
 
